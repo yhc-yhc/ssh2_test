c1d4e250410b22489a34847c406f31c385ee2063
josh <josh.zhu@pictureworks.biz>
2017-04-26 14:22:58 +0800

    josh

---
+++ conn/reports.js
@@ -0,0 +1,13 @@
+module.exports = {
+	url: 'git@172.16.60.9:josh/reports.git',
+	// tar_param: 'dist',
+	servers: [
+		{
+			host: '52.74.193.198',
+			port: 22,
+			username: 'root',
+			password: 'hkdlbca!93456',
+			fly_params: 'NODE_ENV=hk'
+		}
+	]
+}
