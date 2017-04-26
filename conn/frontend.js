f1189dbc96a267ac55087c0236e97d7a4770dce6
josh <josh.zhu@pictureworks.biz>
2017-04-26 14:13:03 +0800

    josh

---
+++ conn/frontend.js
@@ -0,0 +1,27 @@
+module.exports = {
+	url: 'git@172.16.60.9:josh/frontend.git',
+	tar_param: 'dist',
+	servers: [
+		{
+			host: '10.10.30.226',
+			port: 22,
+			username: 'clientadmin',
+			password: 'p@w.T&c',
+			fly_params: ''
+		},
+		{
+			host: '10.10.30.227',
+			port: 22,
+			username: 'clientadmin',
+			password: 'p@w.T&c',
+			fly_params: ''
+		},
+		{
+			host: '10.10.30.229',
+			port: 22,
+			username: 'clientadmin',
+			password: 'p@w.T&c',
+			fly_params: ''
+		}
+	]
+}
